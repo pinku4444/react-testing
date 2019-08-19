b19386b528059c31596d788c4e6d7e4f141264b7
pinku4444 <pinku.kumar@successive.tech>
2019-08-19 13:02:01 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import  Header from './componets/header/index'
+import './app.scss'
 
 function App() {
   return (
     <div className="App">
-      <p>Test</p>
+      <Header />
     </div>
   );
 }
---
+++ src/componets/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss'
+
+const Header = () => {
+    return (
+        <header>
+            <p className="wrap">Header</p>
+        </header>
+    );
+};
+
+export default Header;
